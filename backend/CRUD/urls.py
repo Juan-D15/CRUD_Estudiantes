bc93c6250c52bbd1b8ed3258ee9e83c5ac553e27
Juan Díaz <jchendiaz15@gmail.com>
2025-09-07 14:21:35 -0600

    Urls de los templates

--- backend/CRUD/urls.py
+++ backend/CRUD/urls.py
@@ -1,22 +1,19 @@
-"""
-URL configuration for CRUD project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from estudiantes import views as api
+from estudiantes import views_pages as pages
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", pages.home_page, name="home"),
+    path("registrar/", pages.registrar_page, name="registrar"),
+    path("actualizar/", pages.actualizar_page, name="actualizar"),
+    path("eliminar/", pages.eliminar_page, name="eliminar"),
+    path("listado/", pages.listado_page, name="listado"),
+
+    path("api/estudiantes", api.api_listar),
+    path("api/estudiantes/<int:id_est>", api.api_detalle),
+    path("api/estudiantes/create", api.api_crear),
+    path("api/estudiantes/<int:id_est>/update", api.api_actualizar),
+    path("api/estudiantes/<int:id_est>/delete", api.api_eliminar),
 ]
