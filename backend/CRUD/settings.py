3209cab8ef24c8ced2e96c4c0e3645cfbbb6a79d
Juan Díaz <jchendiaz15@gmail.com>
2025-09-18 17:55:05 -0600

    Ajustes en variables de entorno

--- backend/CRUD/settings.py
+++ backend/CRUD/settings.py
@@ -23,7 +23,7 @@ load_dotenv(BASE_DIR / ".env")  # carga variables
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uw=!+^17^&)^r374=*j&m13o6u2^4rgu=n-f2vo#6v_q*or9+t'
+SECRET_KEY = os.getenv('KEY_DJANGO')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
