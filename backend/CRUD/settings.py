6235d8b7c24b30044cee963bbf96dd04c53ef483
Juan Díaz <jchendiaz15@gmail.com>
2025-09-20 21:18:40 -0600

    Cambios en las llamadas a las variables de entorno

--- backend/CRUD/settings.py
+++ backend/CRUD/settings.py
@@ -31,7 +31,7 @@ RECAPTCHA_SECRET_KEY = os.getenv("RECAPTCHA_SECRET_KEY")
 #EMAIL BACKEND
 EMAIL_BACKEND = os.getenv("EMAIL_BACKEND", "django.core.mail.backends.smtp.EmailBackend")
 EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = int(os.getenv("EMAIL_PORT", "2525"))
+EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "true").lower() == "true"
