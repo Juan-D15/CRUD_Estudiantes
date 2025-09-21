90c76faf8f7cec270b0420131bbbcba994e40183
Juan Díaz <jchendiaz15@gmail.com>
2025-09-20 20:45:45 -0600

    Nuevas urls para la recuperación de contraseñas

--- backend/CRUD/urls.py
+++ backend/CRUD/urls.py
@@ -21,7 +21,14 @@ urlpatterns = [
     path("login/admin/", api.login_admin, name="admin_login"),
     path("login/secretario/", api.login_secretario, name="secretario_login"),
     path("logout/", api.logout_view, name="logout"),
-
+    
+    # Flujo Reset
+    path("auth/reset/send-code", api.reset_send_code, name="reset_send_code"),
+    path("auth/reset/verify",     api.reset_verify_code, name="reset_verify_code"),
+    path("auth/reset/commit",     api.reset_commit,      name="reset_commit"),
+    path("reset/admin",           pages.admin_reset_page,      name="admin_reset_page"),
+    path("reset/secretario",      pages.secretario_reset_page, name="secretario_reset_page"),
+    
     # API
     path("api/estudiantes", api.api_listar),
     path("api/estudiantes/<int:id_est>", api.api_detalle),
